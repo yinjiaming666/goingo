de12908d4ed03f9d506c9e0ec097101e56693e4d
yin <1830959054@qq.com>
2024-10-18 15:39:38 +0800

    新增 RandSlice

--- tools/random/random.go
+++ tools/random/random.go
@@ -29,3 +29,13 @@ func Number(start, end int) int {
 	}
 	return rand.Intn(end-start) + start // (end-start)+start
 }
+
+// RandSlice 随机弹出切片中的元素
+func RandSlice[T any](slice []T) (T, int) {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	if len(slice) == 0 {
+		panic("empty slice")
+	}
+	randIndex := r.Intn(len(slice))
+	return slice[randIndex], randIndex
+}
