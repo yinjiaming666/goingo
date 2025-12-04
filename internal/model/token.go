d311d430397e987d17e713ebee05f1f420ded73f
yin <1830959054@qq.com>
2025-12-04 15:52:38 +0800

    refactor(core): 重构代码导入顺序和注释优化
    - 统一调整多个文件中的包导入顺序，将标准库与第三方库分组
    - 优化结构体字段注释格式，提升可读性
    - 注释掉部分未使用的初始化逻辑，减少冗余代码
    - 移除无用的空行和多余导入，保持代码整洁
    - 标准化模型字段定义格式，增强一致性

--- internal/model/token.go
+++ internal/model/token.go
@@ -4,15 +4,15 @@ import "app/tools/jwt"
 
 type Token struct {
 	*MysqlBaseModel `gorm:"-:all"` // -:all 无读写迁移权限，该字段不在数据库中
-	Id              uint   `json:"id" gorm:"primaryKey;type:INT(8) UNSIGNED NOT NULL AUTO_INCREMENT"`
-	Uid             uint   `json:"uid" gorm:"type:INT(8) UNSIGNED NOT NULL;default:0"`
-	ExpireTime      int64  `json:"expire_time" gorm:"type:BIGINT UNSIGNED NOT NULL;default:0"`
-	Token           string `json:"token" gorm:"type:VARCHAR(255) NOT NULL;default:''"`
-	Type            string `json:"type" gorm:"type:VARCHAR(255) NOT NULL;default:''"`
-	DeviceType      uint8  `json:"device_type" gorm:"type:TINYINT(8) NOT NULL;default:0"`  // 设备类型
-	DeviceId        string `json:"device_id" gorm:"type:VARCHAR(255) NOT NULL;default:''"` // 设备id
-	CreateTime      int64  `json:"create_time" gorm:"autoCreateTime;type:BIGINT UNSIGNED NOT NULL;"`
-	CreateTimeStr   string `json:"create_time_str" gorm:"-:all"`
+	Id              uint           `json:"id" gorm:"primaryKey;type:INT(8) UNSIGNED NOT NULL AUTO_INCREMENT"`
+	Uid             uint           `json:"uid" gorm:"type:INT(8) UNSIGNED NOT NULL;default:0"`
+	ExpireTime      int64          `json:"expire_time" gorm:"type:BIGINT UNSIGNED NOT NULL;default:0"`
+	Token           string         `json:"token" gorm:"type:VARCHAR(255) NOT NULL;default:''"`
+	Type            string         `json:"type" gorm:"type:VARCHAR(255) NOT NULL;default:''"`
+	DeviceType      uint8          `json:"device_type" gorm:"type:TINYINT(8) NOT NULL;default:0"`  // 设备类型
+	DeviceId        string         `json:"device_id" gorm:"type:VARCHAR(255) NOT NULL;default:''"` // 设备id
+	CreateTime      int64          `json:"create_time" gorm:"autoCreateTime;type:BIGINT UNSIGNED NOT NULL;"`
+	CreateTimeStr   string         `json:"create_time_str" gorm:"-:all"`
 }
 
 func (t *Token) CheckToken(token string, jType jwt.JType) *Token {
