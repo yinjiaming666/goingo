5a2806f8b3cdceb3425f4f01b8b7dadc5defca86
yin <1830959054@qq.com>
2025-12-10 14:42:07 +0800

    feat(mysql): 调整数据库连接池配置
    - 将最大闲置连接数从 10 增加到 100
    - 将最大打开连接数从 100 设置为无限制 (0)
    - 将连接最大生命周期从 10 秒延长至 1 小时

--- internal/model/mysql.go
+++ internal/model/mysql.go
@@ -55,13 +55,13 @@ func InitDb(c *DbConf) {
 
 	sqlDB, _ := db.DB()
 	// SetMaxIdleCons 设置连接池中的最大闲置连接数。
-	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxIdleConns(100)
 
 	// SetMaxOpenCons 设置数据库的最大连接数量。
-	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetMaxOpenConns(0)
 
-	// SetConnMaxLifetiment 设置连接的最大可复用时间。
-	sqlDB.SetConnMaxLifetime(10 * time.Second)
+	// SetConnMaxLifetime 设置连接的最大可复用时间。
+	sqlDB.SetConnMaxLifetime(time.Hour)
 
 }
 
